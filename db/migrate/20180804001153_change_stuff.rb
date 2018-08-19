facf68fa077e980a9114dbb27490ff22a03c0948
corear <c_orear@icloud.com>
2018-08-19 21:26:48 +0000

    g

--- db/migrate/20180804001153_change_stuff.rb
+++ db/migrate/20180804001153_change_stuff.rb
@@ -1,6 +1,6 @@
 class ChangeStuff < ActiveRecord::Migration
   def change
-    change_column :groups, :owner, :integer
+    change_column :groups, :owner, :integer, using: 'owner::integer'
     remove_index :groups, :name
     add_index :groups, :name, unique: true
   end
